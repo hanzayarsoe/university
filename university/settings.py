ef2979885166d704af4dd04b32c2a083febed7ba
hanzayarsoe <hanzayarsoe404@gmail.com>
2024-11-21 20:12:18 +0630

    empty app password field

--- university/settings.py
+++ university/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hanzayarsoe404@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = "enter-your-app-password-here"
